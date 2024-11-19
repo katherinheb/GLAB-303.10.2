747bff63affdbeef236a2d60a13faaae496c768a
katherinheb <katherinheb@gmail.com>
2024-11-19 11:11:30 -0500

    Update Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,9 +1,30 @@
+public class Cylinder extends Circle {
+   private final double PI = Math.PI  ;
 
-public class Cylinder {
+   public Cylinder(double radius, double height) {
+       super(radius, height);
+       // TODO Auto-generated constructor stub
+   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   public Cylinder(double radius) {
+       super(radius);
+   }
 
-	}
+   /** Returns the volume of this cylinder */
+   public double getVolumne() {
+       return  (PI*Math.pow(super.radius, 2)) * super.height;
+   }
 
+   public double getSurfaceArea() {
+       return 2.0 * Math.PI*super.radius*super.height;
+   }
+   @Override
+   public void displayshapName() {
+       System.out.println("Drawing a Cylinder for radius " + super.radius);
+   }
+   public String toString()
+   {
+       return "radius is: " + super.radius +"height is : " + super.height;
+
+   }
 }
